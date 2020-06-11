60e0d243d4121438ce9abab762a95d74a45b788f
PhilipPhil <philipgarabandic@gmail.com>
2020-06-10 23:01:57 -0400

    no message

--- src/popup.js
+++ src/popup.js
@@ -18,6 +18,14 @@ function showMap(position) {
     setData(lat, lon)
 }
 
+function setImage(lat, lon) {
+    document.getElementById('geoLocation').innerText = "Lat " + lat.toFixed(6) + ", Lon " + lon.toFixed(6)
+    apikey = 'iJqafJbROElNaKRqqk24Ot5eN6WTYCqYFdteeYz5'
+    imgURL = 'https://api.nasa.gov/planetary/earth/imagery?lon=' + lon + '&lat=' + lat + '&dim=0.1&api_key=' + apikey
+    document.getElementById('geoImage').src = imgURL
+
+}
+
 function setData(lat, lon) {
     apikey = '3e7cd6048bd8114abedeee14fcc11575'
     fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + apikey)
@@ -25,34 +33,12 @@ function setData(lat, lon) {
         .then(data => getData(data));
 }
 
-function setImage(lat, lon) {
-    document.getElementById('geoLocation').innerText = "Lat " + lat.toFixed(6) + ", Lon " + lon.toFixed(6)
-    if (lat.toFixed(4) == localStorage.getItem('lat') && lat.toFixed(4) == localStorage.getItem('lat')) {
-        document.getElementById('geoImage').src = localStorage.getItem("geoImage")
-    } else {
-        apikey = 'iJqafJbROElNaKRqqk24Ot5eN6WTYCqYFdteeYz5'
-        imgURL = 'https://api.nasa.gov/planetary/earth/imagery?lon=' + lon + '&lat=' + lat + '&dim=0.1&api_key=' + apikey
-        document.getElementById('geoImage').src = imgURL
-        localStorage.setItem("lat", lat.toFixed(4))
-        localStorage.setItem("lon", lon.toFixed(4))
-        localStorage.setItem("geoImage", imgURL)
-    }
-
-
-}
-
 function getData(data) {
-    celsius = Math.round(data["main"]["temp"] - 273.15)
     cel = Math.round(data["main"]["temp"] - 273.15) + "°C"
     far = Math.round((data["main"]["temp"] - 273.15) * 9 / 5 + 32) + "°F"
     sunrise = "Sunrise " + getDateString(data["sys"]["sunrise"] * 1000);
     sunset = "Sunset " + getDateString(data["sys"]["sunset"] * 1000);
     city = data["name"]
-    description = data["weather"][0]['description']
-    description = description.charAt(0).toUpperCase() + description.slice(1)
-    data = { "cel": cel, "far": far, "sunrise": sunrise, "sunset": sunset, "city": city, "description": description }
-    localStorage["newData"] = JSON.stringify(data)
-    document.getElementById('description').innerText = description
     document.getElementById('cel').innerText = cel
     document.getElementById('far').innerText = far
     document.getElementById('sunrise').innerText = sunrise

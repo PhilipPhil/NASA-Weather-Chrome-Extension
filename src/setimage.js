bb6be8e30d0321b807f2a6bc1e2a7d1ed1ab1448
PhilipPhil <philipgarabandic@gmail.com>
2020-06-10 21:31:13 -0400

    Fixes on load faster. Saves first load of data to local memory for faster load.

--- src/setimage.js
+++ src/setimage.js
@@ -15,6 +15,7 @@ function addBackGround(data, dt) {
     if (data["media_type"] == "image") {
         document.getElementById('title').innerText = data['title']
         document.body.style.backgroundImage = "url('" + data['hdurl'] + "')"
+        localStorage.setItem("APOD", "url('" + data['hdurl'] + "')");
     } else {
         dt.setDate(dt.getDate() - 1);
         setImage(dt)
@@ -25,4 +26,11 @@ var APIUrl = 'https://api.nasa.gov/planetary/apod?date='
 var key = '&api_key=iJqafJbROElNaKRqqk24Ot5eN6WTYCqYFdteeYz5'
 dt = new Date()
 document.getElementById('date').innerText = dt.toDateString()
-setImage(dt)
+currentDMY = dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate()
+if (currentDMY == localStorage.getItem('lastDMY')) {
+    document.getElementById('title').innerText = localStorage.getItem('lastDMY')
+    document.body.style.backgroundImage = localStorage.getItem('APOD')
+} else {
+    setImage(dt)
+    localStorage.setItem('lastDMY',currentDMY)
+}

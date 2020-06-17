5bdc6d1588a18451c324f667bf6c74fcdb95533b
PhilipPhil <philipgarabandic@gmail.com>
2020-06-17 19:31:46 -0400

    no message

--- src/goto.js
+++ src/goto.js
@@ -1,7 +1,7 @@
 document.getElementById('goToApps').addEventListener('click', function(e) {
-    chrome.tabs.update({"url":"chrome://apps"})
+    chrome.tabs.update({url:"chrome://apps"})
 
 })
 document.getElementById('goToDefault').addEventListener('click', function(e) {
-    chrome.tabs.update({"url":"chrome-search://local-ntp/local-ntp.html"})
+    chrome.tabs.update({url:"chrome-search://local-ntp/local-ntp.html"})
 })

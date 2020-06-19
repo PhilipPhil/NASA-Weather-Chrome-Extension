1ba3f017f79a3a2484e2421cfc913235a6fe43ae
PhilipPhil <philipgarabandic@gmail.com>
2020-06-19 14:45:00 -0400

    Fixes image modal

--- src/setimage.js
+++ src/setimage.js
@@ -20,15 +20,22 @@ function addBackGround(data, dateDirection) {
     if (data["media_type"] == "image") {
         title = data["title"];
         imgURL = "url('" + data["hdurl"] + "')";
-
         imgdate = data["date"]
-        
         explanation = data["explanation"]
+        yy = positionDate.getFullYear().toString().substring(0,2)
+        pad = "00"
+        mm = (positionDate.getMonth() + 1).toString()
+        mm = pad.substring(0, pad.length - mm.length) + mm
+        dd = positionDate.getDate().toString()
+        dd = pad.substring(0, pad.length - dd.length) + dd
+        apodURL = 'https://apod.nasa.gov/apod/ap'+yy+mm+dd+'.html'
 
+        document.getElementById("apodURL").href = apodURL
         document.getElementById("modaltitle").innerText = title;
         document.getElementById("modaldate").innerText = imgdate;
         document.getElementById("modaldescription").innerText = explanation;
         document.getElementById("modalimage").src = data["hdurl"]
+        document.getElementById("modalhdurl").href = data["hdurl"]
 
         document.getElementById("title").innerText = title;
         document.body.style.backgroundImage = imgURL;
@@ -39,7 +46,7 @@ function addBackGround(data, dateDirection) {
 }
 
 function initalImageLoad() {
-    document.getElementById("right").style.display = "none";
+    document.getElementById("right").style.visibility = "hidden";
     setImage(-1);
 }
 
@@ -53,7 +60,7 @@ positionDate.setHours(positionDate.getHours() - 8); // set to pacific time when 
 initalImageLoad()
 
 document.getElementById("left").addEventListener("click", function (e) {
-    document.getElementById("right").style.display = "block";
+    document.getElementById("right").style.visibility = "visible";
     dateDirection = -1;
     positionDate.setDate(positionDate.getDate() + dateDirection);
     setImage(dateDirection);
@@ -66,13 +73,8 @@ document.getElementById("right").addEventListener("click", function (e) {
     if (positionDate.getFullYear() == currDate.getFullYear() &&
         positionDate.getMonth() == currDate.getMonth() &&
         positionDate.getDate() == currDate.getDate()) {
-        document.getElementById("right").style.display = "none";
+        document.getElementById("right").style.visibility = "hidden";
     }
     setImage(dateDirection);
 
 });
-
-
-$('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-  })

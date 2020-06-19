012994030ff70bb55e95f145f9628d076003cf9e
PhilipPhil <philipgarabandic@gmail.com>
2020-06-19 16:18:12 -0400

    Adds left right arrow change image interation

--- src/setimage.js
+++ src/setimage.js
@@ -1,7 +1,7 @@
 function setImage(dateDirection) {
     try {
         ymd = positionDate.getFullYear() + "-" + (positionDate.getMonth() + 1) + "-" + positionDate.getDate();
-        // alert(APIUrl + ymd + key)
+        // alert(APIUrl + ymd + key);
         fetch(APIUrl + ymd + key)
             .then((response) => response.json())
             .then((data) => addBackGround(data, dateDirection));
@@ -20,36 +20,36 @@ function addBackGround(data, dateDirection) {
     if (data["media_type"] == "image") {
         title = data["title"];
         imgURL = "url('" + data["hdurl"] + "')";
-        imgdate = data["date"]
-        explanation = data["explanation"]
-        yy = positionDate.getFullYear().toString().substring(0, 2)
-        pad = "00"
-        mm = (positionDate.getMonth() + 1).toString()
-        mm = pad.substring(0, pad.length - mm.length) + mm
-        dd = positionDate.getDate().toString()
-        dd = pad.substring(0, pad.length - dd.length) + dd
-        apodURL = 'https://apod.nasa.gov/apod/ap' + yy + mm + dd + '.html'
+        imgdate = data["date"];
+        explanation = data["explanation"];
+        yy = positionDate.getFullYear().toString().substring(0, 2);
+        pad = "00";
+        mm = (positionDate.getMonth() + 1).toString();
+        mm = pad.substring(0, pad.length - mm.length) + mm;
+        dd = positionDate.getDate().toString();
+        dd = pad.substring(0, pad.length - dd.length) + dd;
+        apodURL = "https://apod.nasa.gov/apod/ap" + yy + mm + dd + ".html";
 
-        hdurl = data["hdurl"]
+        hdurl = data["hdurl"];
 
-        document.getElementById("apodURL").href = apodURL
+        document.getElementById("apodURL").href = apodURL;
         document.getElementById("modaltitle").innerText = title;
         document.getElementById("title").innerText = title;
         document.getElementById("modaldate").innerText = imgdate;
         document.getElementById("modaldescription").innerText = explanation;
-        document.getElementById("modalimage").src = hdurl
-        document.getElementById("modalimage").alt = title
-        document.getElementById("modalhdurl").href = hdurl
+        document.getElementById("modalimage").src = hdurl;
+        document.getElementById("modalimage").alt = title;
+        document.getElementById("modalhdurl").href = hdurl;
         document.body.style.backgroundImage = imgURL;
 
-        if (posYMD != localStorage['lastupdYMD']) {
-            localStorage['apodURL'] = apodURL
-            localStorage['title'] = title
-            localStorage['imgdate'] = imgdate
-            localStorage['explanation'] = explanation
-            localStorage['hdurl'] = hdurl
-            localStorage['imgURL'] = imgURL
-            localStorage['lastupdYMD'] = posYMD
+        if (posYMD != localStorage["lastupdYMD"]) {
+            localStorage["apodURL"] = apodURL;
+            localStorage["title"] = title;
+            localStorage["imgdate"] = imgdate;
+            localStorage["explanation"] = explanation;
+            localStorage["hdurl"] = hdurl;
+            localStorage["imgURL"] = imgURL;
+            localStorage["lastupdYMD"] = posYMD;
         }
     } else {
         positionDate.setDate(positionDate.getDate() + dateDirection);
@@ -58,7 +58,7 @@ function addBackGround(data, dateDirection) {
 }
 
 const APIUrl = "https://api.nasa.gov/planetary/apod?date=";
-const key = '&api_key=iJqafJbROElNaKRqqk24Ot5eN6WTYCqYFdteeYz5'
+const key = "&api_key=iJqafJbROElNaKRqqk24Ot5eN6WTYCqYFdteeYz5";
 
 var positionDate = new Date();
 positionDate = new Date(positionDate.toUTCString());
@@ -68,16 +68,17 @@ const posYMD = positionDate.getFullYear() + "-" + (positionDate.getMonth() + 1) 
 
 document.getElementById("right").style.visibility = "hidden";
 
-if (posYMD == localStorage['lastupdYMD']) {
-    document.getElementById("apodURL").href = localStorage['apodURL']
-    document.getElementById("modaltitle").innerText = localStorage['title'];
-    document.getElementById("title").innerText = localStorage['title'];
-    document.getElementById("modaldate").innerText = localStorage['imgdate'];
-    document.getElementById("modaldescription").innerText = localStorage['explanation'];
-    document.getElementById("modalimage").src = localStorage['hdurl']
-    document.getElementById("modalimage").alt = localStorage['title']
-    document.getElementById("modalhdurl").href = localStorage['hdurl']
-    document.body.style.backgroundImage = localStorage['imgURL'];
+if (posYMD == localStorage["lastupdYMD"]) {
+    document.getElementById("apodURL").href = localStorage["apodURL"];
+    document.getElementById("modaltitle").innerText = localStorage["title"];
+    document.getElementById("title").innerText = localStorage["title"];
+    document.getElementById("modaldate").innerText = localStorage["imgdate"];
+    document.getElementById("modaldescription").innerText =
+        localStorage["explanation"];
+    document.getElementById("modalimage").src = localStorage["hdurl"];
+    document.getElementById("modalimage").alt = localStorage["title"];
+    document.getElementById("modalhdurl").href = localStorage["hdurl"];
+    document.body.style.backgroundImage = localStorage["imgURL"];
 } else {
     setImage(-1);
 }
@@ -93,10 +94,42 @@ document.getElementById("right").addEventListener("click", function (e) {
     dateDirection = 1;
     positionDate.setDate(positionDate.getDate() + dateDirection);
     currDate = new Date();
-    if (positionDate.getFullYear() == currDate.getFullYear() &&
-        positionDate.getMonth() == currDate.getMonth() &&
-        positionDate.getDate() == currDate.getDate()) {
-        document.getElementById("right").style.visibility = "hidden";
+    if (positionDate > currDate) {
+        positionDate.setDate(positionDate.getDate() - 1);
+    } else {
+        if (positionDate.getFullYear() == currDate.getFullYear() &&
+            positionDate.getMonth() == currDate.getMonth() &&
+            positionDate.getDate() == currDate.getDate()) {
+            document.getElementById("right").style.visibility = "hidden";
+        }
+        setImage(dateDirection);
     }
-    setImage(dateDirection);
+});
+
+document.addEventListener("keydown", function (event) {
+    const key = event.key;
+    switch (key) {
+        case "ArrowRight":
+            dateDirection = 1;
+            positionDate.setDate(positionDate.getDate() + dateDirection);
+            currDate = new Date();
+            if (positionDate > currDate) {
+                positionDate.setDate(positionDate.getDate() - 1);
+                break;
+            } else {
+                if (positionDate.getFullYear() == currDate.getFullYear() &&
+                    positionDate.getMonth() == currDate.getMonth() &&
+                    positionDate.getDate() == currDate.getDate()) {
+                    document.getElementById("right").style.visibility = "hidden";
+                }
+                setImage(dateDirection);
+                break;
+            }
+        case "ArrowLeft":
+            document.getElementById("right").style.visibility = "visible";
+            dateDirection = -1;
+            positionDate.setDate(positionDate.getDate() + dateDirection);
+            setImage(dateDirection);
+            break;
+    }
 });

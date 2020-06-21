6f5cf6eb19b63dd0fd36d0f011122b2ee807932f
PhilipPhil <philipgarabandic@gmail.com>
2020-06-21 01:23:52 -0400

    no message

--- src/popup.js
+++ src/popup.js
@@ -1,52 +1,50 @@
-function getDateString(unixTime) {
-    date = new Date(unixTime);
-    hours = date.getHours();
-    minutes = "0" + date.getMinutes();
-    if (hours > 12) {
-        hours = hours - 12
-        unixTime = hours + ':' + minutes.substr(-2) + " p.m.";
-    } else {
-        unixTime = hours + ':' + minutes.substr(-2) + " a.m.";
-    }
-    return unixTime
+function getTime(dt) {
+    time = dt.toLocaleTimeString().split(":")[0] + ":" + dt.toLocaleTimeString().split(":")[1] + ' ' + dt.toLocaleTimeString().split(" ")[1]
+    return time
 }
 
 function showMap(position) {
     lat = position.coords.latitude
     lon = position.coords.longitude
-    setImage(lat, lon)
-    setData(lat, lon)
+    setCurrent(lat, lon)
 }
 
-function setImage(lat, lon) {
-    document.getElementById('geoLocation').innerText = "Lat " + lat.toFixed(6) + ", Lon " + lon.toFixed(6)
-    apikey = 'iJqafJbROElNaKRqqk24Ot5eN6WTYCqYFdteeYz5'
-    imgURL = 'https://api.nasa.gov/planetary/earth/imagery?lon=' + lon + '&lat=' + lat + '&dim=0.1&api_key=' + apikey
-    document.getElementById('geoImage').src = imgURL
-
-}
-
-function setData(lat, lon) {
+function setCurrent(lat, lon) {
     apikey = '3e7cd6048bd8114abedeee14fcc11575'
-    fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + apikey)
+    const urlapi = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&exclude=minutely,hourly,daily&appid=${apikey}`
+    fetch(urlapi)
         .then(response => response.json())
-        .then(data => getData(data));
+        .then(data => getCurrent(data));
 }
 
-function getData(data) {
-    description = data["weather"][0]['description']
-    description = description.charAt(0).toUpperCase() + description.slice(1)
-    city = data["name"]
-    cel = Math.round(data["main"]["temp"] - 273.15) + "°C"
-    far = Math.round((data["main"]["temp"] - 273.15) * 9 / 5 + 32) + "°F"
-    sunrise = "Sunrise " + getDateString(data["sys"]["sunrise"] * 1000);
-    sunset = "Sunset " + getDateString(data["sys"]["sunset"] * 1000);
-    document.getElementById('city').innerText = city;
-    document.getElementById('description').innerText = description
-    document.getElementById('cel').innerText = cel
-    document.getElementById('far').innerText = far
-    document.getElementById('sunrise').innerText = sunrise
-    document.getElementById('sunset').innerText = sunset
+function getCurrent(data) {
+    dt = new Date()
+    description = data.weather[0].description.replace(/(^\w{1})|(\s{1}\w{1})/g, match => match.toUpperCase());
+    day = dt.toString().split(' ')[0];
+    time = getTime(dt)
+    currentweathericon = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`
+    celsius = Math.round(data.main.temp - 273.15)
+    sunrise = (new Date(data.sys.sunrise * 1000)).toLocaleTimeString()
+    sunset = (new Date(data.sys.sunset * 1000)).toLocaleTimeString()
+    humidity = data.main.humidity
+    windspeed = data.wind.speed
+    city = data.name
+    min = Math.round(data.main.temp_min - 273.15)
+    max = Math.round(data.main.temp_max - 273.15)
+
+    document.getElementById('current-weather-description').innerText = day + ', ' + time + ', ' + description
+    document.getElementById('current-weather-icon').src = currentweathericon
+    document.getElementById('current-temp').innerText = celsius
+    document.getElementById('current-sunrise').innerText = sunrise
+    document.getElementById('current-sunset').innerText = sunset
+    document.getElementById('current-humidity').innerText = humidity
+    document.getElementById('current-windspeed').innerText = windspeed
+    document.getElementById('city').innerText = city
+    document.getElementById('current-min').innerText = min
+    document.getElementById('current-max').innerText = max
 }
+
+
+
 
 navigator.geolocation.getCurrentPosition(showMap);
